d875034f648c8f8ad6430e9af2f9384a771f281f
Jim Grimes <jimgrimes86@gmail.com>
2023-10-09 10:04:15 -0400

    DONE: stocks render on page, click to buy stock, click to sell stock

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const stockDatabase = "http://localhost:3001/stocks";
+  const [allStocks, setAllStocks] = useState([]);
+  const [boughtStocks, setBoughtStocks] = useState([]);
+
+  useEffect(fetchStocks, [])
+
+  function fetchStocks() {
+    fetch(stockDatabase)
+    .then(response => response.json())
+    .then(data => setAllStocks(() => data))
+  }
+
+  function clickOnStock(stock) {
+    const alreadyBoughtStock = boughtStocks.find((oneStock) => {
+      if (oneStock === stock) {return true}
+    })
+    if (alreadyBoughtStock) {
+      sellStock(stock)
+    } else {buyStock(stock)}
+  }
+  
+  function buyStock(stock) {
+    setBoughtStocks([...boughtStocks, stock])
+  }
+
+  function sellStock(stock) {
+    setBoughtStocks(boughtStocks.filter((oneStock) => {
+      if (oneStock !== stock) {
+        return true
+      }}))
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={allStocks} clickOnStock={clickOnStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer boughtStocks={boughtStocks} clickOnStock={clickOnStock} />
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks, clickOnStock}) {
+
+  const stockList = stocks.map((stock) => {
+    return <Stock key={stock.id} stock={stock} clickOnStock={clickOnStock} />
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList}
     </div>
   );
 }
